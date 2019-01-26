2876b3c4301e6fdabce3c388a67867aa750731c5
Timo Piechotta <piechotta@mediamarktsaturn.com>
2019-01-26 15:44:57 +0100

    Add Address To Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_26_141141) do
+ActiveRecord::Schema.define(version: 2019_01_26_142657) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
 end
